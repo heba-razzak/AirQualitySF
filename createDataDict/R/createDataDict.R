81b97c98a1f24c12aa1c1ed177b833592058282c
heba-razzak <heba.razzak@hotmail.com>
2024-03-18 15:30:05 -0700

    fix description error

--- createDataDict/R/createDataDict.R
+++ createDataDict/R/createDataDict.R
@@ -12,30 +12,7 @@
 #' @export
 #'
 #' @examples
-#' >print_descriptions_df(mtcars)
-#'
-#' descriptions = data.frame(Variable = c('mpg',
-#' 'cyl',
-#' 'disp',
-#' 'hp',
-#' 'drat',
-#' 'wt',
-#' 'qsec',
-#' 'vs',
-#' 'am',
-#' 'gear',
-#' 'carb'),
-#' Description = c('mpg_description',
-#'                 'cyl_description',
-#'                 'disp_description',
-#'                 'hp_description',
-#'                 'drat_description',
-#'                 'wt_description',
-#'                 'qsec_description',
-#'                 'vs_description',
-#'                 'am_description',
-#'                 'gear_description',
-#'                 'carb_description'))
+#' print_descriptions_df(mtcars)
 print_descriptions_df <- function(data) {
   # Get variable names
   variables <- names(data)
@@ -73,23 +50,8 @@ print_descriptions_df <- function(data) {
 #' @export
 #'
 #' @examples
+#' descriptions = print_descriptions_df(mtcars)
 #' print_data_dict(mtcars, title = "Cars", descriptions = descriptions)
-#' ## **Cars**
-#' **Number of Rows**: `32`
-#'
-#' |Variable |Type    | NA| %NA| Unique|Description      |
-#'   |:--------|:-------|--:|---:|------:|:----------------|
-#'   |mpg      |numeric |  0|   0|     25|mpg_description  |
-#'   |cyl      |numeric |  0|   0|      3|cyl_description  |
-#'   |disp     |numeric |  0|   0|     27|disp_description |
-#'   |hp       |numeric |  0|   0|     22|hp_description   |
-#'   |drat     |numeric |  0|   0|     22|drat_description |
-#'   |wt       |numeric |  0|   0|     29|wt_description   |
-#'   |qsec     |numeric |  0|   0|     30|qsec_description |
-#'   |vs       |numeric |  0|   0|      2|vs_description   |
-#'   |am       |numeric |  0|   0|      2|am_description   |
-#'   |gear     |numeric |  0|   0|      3|gear_description |
-#'   |carb     |numeric |  0|   0|      6|carb_description |
 print_data_dict <- function(data, title = "", descriptions = NULL) {
   # Load packages
   if (!require("dplyr")) {
@@ -125,9 +87,7 @@ print_data_dict <- function(data, title = "", descriptions = NULL) {
   if (!is.null(descriptions)) {
     descriptions$Variable <- as.character(descriptions$Variable)
     descriptions$Description <- as.character(descriptions$Description)
-    dict_table <- dict_table %>% left_join(descriptions, by = "Variable")
-    # Replace NA with blanks
-    dict_table <- dict_table[is.na(dict_table)] <- ""
+    dict_table <- dict_table %>% dplyr::left_join(descriptions, by = "Variable")
   } else {
     dict_table$Description <- ""
   }
